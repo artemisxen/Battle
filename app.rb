acad8c0f245549c09eb2864acacb7ebe1d4f8f4f
artemis <artemisxen@gmail.com>
2017-07-20 17:00:26 +0100

    global variables removed

--- app.rb
+++ app.rb
@@ -26,33 +26,31 @@ end
   end
 
   post '/names' do
-    $game = Game.start(Player.new(params[:player_1]), Player.new(params[:player_2]))
     Game.start(Player.new(params[:player_1]), Player.new(params[:player_2]))
     session[:game] = Game.current
     redirect '/play'
   end
 
   get '/play' do
-    p @game
-    @player_1 = $game.player_1.name
-    @player_2 = $game.player_2.name
-    @player_1_points = $game.player_1.hp
-    @player_2_points = $game.player_2.hp
-    @turn = $game.turn.name
+    @player_1 = @game.player_1.name
+    @player_2 = @game.player_2.name
+    @player_1_points = @game.player_1.hp
+    @player_2_points = @game.player_2.hp
+    @turn = @game.turn.name
     @latest_event = session[:latest_event]
     erb(:play)
   end
 
   post '/attack_1' do
-    $game.attack($game.player_2)
-    session[:latest_event] = "#{$game.player_1.name} attacked #{$game.player_2.name}"
-    check_if_game_over($game.player_2)
+    @game.attack(@game.player_2)
+    session[:latest_event] = "#{@game.player_1.name} attacked #{@game.player_2.name}"
+    check_if_game_over(@game.player_2)
   end
 
   post '/attack_2' do
-    $game.attack($game.player_1)
-    session[:latest_event] = "#{$game.player_2.name} attacked #{$game.player_1.name}"
-    check_if_game_over($game.player_1)
+    @game.attack(@game.player_1)
+    session[:latest_event] = "#{@game.player_2.name} attacked #{@game.player_1.name}"
+    check_if_game_over(@game.player_1)
   end
 
   get '/lose' do
